f296264008e0810254bdde3a4cc590f50310fe90
shauryamanocha <shaurya.manocha1@gmail.com>
2019-03-12 09:17:54 -0400

    removed manual control

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -59,6 +59,6 @@ public class Elevator extends Subsystem {
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
-    setDefaultCommand(new ManualElevatorControl());
+    // setDefaultCommand(new ManualElevatorControl());
   }
 }
