58c133a60be6ec0e16f34f8b9803123d19988a13
shauryamanocha <shaurya.manocha1@gmail.com>
2019-03-19 17:03:56 -0400

    added gear switcher buttons

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -8,6 +8,10 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.SwitchDriveGear;
+import frc.robot.subsystems.Drive;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -15,4 +19,11 @@ import edu.wpi.first.wpilibj.Joystick;
  */
 public class OI {
   public static Joystick driveStick = new Joystick(RobotMap.DRIVE_JOYSTICK_ID);
+
+  public static Button driveFullSpeed = new JoystickButton(driveStick, 5);//left bumper
+  public static Button driveSlowSpeed = new JoystickButton(driveStick, 6);//right bumper
+  public OI(){
+    driveFullSpeed.whenPressed(new SwitchDriveGear(Drive.FULL_SPEED));
+    driveSlowSpeed.whenPressed(new SwitchDriveGear(Drive.SLOW_SPEED));
+  }
 }
