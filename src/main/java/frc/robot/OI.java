169486b2ad5cb255d9edae47b8f6f18692f9cb36
shauryamanocha <shaurya.manocha1@gmail.com>
2019-03-19 16:57:45 -0400

    added command group and updated OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -16,6 +16,7 @@ import frc.robot.commands.CargoShooter_Start;
 import frc.robot.commands.CargoShooter_Stop;
 import frc.robot.commands.CargoTransporter_Stop;
 import frc.robot.commands.Cargo_Transporter_Start;
+import frc.robot.commands.IntakeCargo;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -23,17 +24,26 @@ import frc.robot.commands.Cargo_Transporter_Start;
  */
 public class OI {
   public static Joystick driveStick = new Joystick(RobotMap.DRIVE_JOYSTICK_ID);
-  private Button intakeButton = new JoystickButton(driveStick,2);
-  private Button cargoShooter = new JoystickButton(driveStick, 4);
-  private Button conveyorButton = new JoystickButton(driveStick,1);
+  private Button cargoShooter = new JoystickButton(driveStick, 3);//B button
+  private Button cargoIntake = new JoystickButton(driveStick,4);//Y button
+  /*Used for manual control of cargo intake
+    private Button conveyorButton = new JoystickButton(driveStick,1);
+    private Button intakeButton = new JoystickButton(driveStick,2);
+  */
   public OI(){
-    intakeButton.whenPressed(new CargoIntake_Start());
-    intakeButton.whenReleased(new CargoIntake_Stop());
+    /*Used for manual control of cargo intake
+        conveyorButton.whenPressed(new Cargo_Transporter_Start());
+        conveyorButton.whenReleased(new CargoTransporter_Stop());
 
+        intakeButton.whenPressed(new CargoIntake_Start());
+        intakeButton.whenReleased(new CargoIntake_Stop());
+
+    */ 
+    //assign buttons their commands
     cargoShooter.whenPressed(new CargoShooter_Start());
     cargoShooter.whenReleased(new CargoShooter_Stop());
-
-    conveyorButton.whenPressed(new Cargo_Transporter_Start());
-    conveyorButton.whenReleased(new CargoTransporter_Stop());
+    //cargo shooter buttons
+    cargoIntake.whenPressed(new IntakeCargo());
+    //cargo intake button
   }
 }
