48a8711c85de6079d3f5193b8f283b7b89fa0028
shauryamanocha <shaurya.manocha1@gmail.com>
2019-03-21 12:59:32 -0400

    added tested endgame functions

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -16,6 +16,7 @@ import frc.robot.commands.CargoShooter_Start;
 import frc.robot.commands.CargoShooter_Stop;
 import frc.robot.commands.CargoTransporter_Stop;
 import frc.robot.commands.Cargo_Transporter_Start;
+import frc.robot.commands.ToggleEndgame;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -26,14 +27,54 @@ public class OI {
   private Button intakeButton = new JoystickButton(driveStick,2);
   private Button cargoShooter = new JoystickButton(driveStick, 4);
   private Button conveyorButton = new JoystickButton(driveStick,1);
+  private Button toggleEndgame = new JoystickButton(driveStick,10);//push right stick
+  public static final int INGAME_MODE = -1;
+  public static final int ENDGAME_MODE = 1;
+  public static int currentMode = -1;
+
   public OI(){
-    intakeButton.whenPressed(new CargoIntake_Start());
-    intakeButton.whenReleased(new CargoIntake_Stop());
-
-    cargoShooter.whenPressed(new CargoShooter_Start());
-    cargoShooter.whenReleased(new CargoShooter_Stop());
-
-    conveyorButton.whenPressed(new Cargo_Transporter_Start());
-    conveyorButton.whenReleased(new CargoTransporter_Stop());
+    updateButtons(INGAME_MODE);
   }
+  public void updateButtons(int buttonMode){
+    currentMode = buttonMode;
+    switch(buttonMode){
+      case OI.INGAME_MODE:
+      intakeButton.whenPressed(new CargoIntake_Start());
+      intakeButton.whenReleased(new CargoIntake_Stop());
+
+      cargoShooter.whenPressed(new CargoShooter_Start());
+      cargoShooter.whenReleased(new CargoShooter_Stop());
+
+      conveyorButton.whenPressed(new Cargo_Transporter_Start());
+      conveyorButton.whenReleased(new CargoTransporter_Stop());
+      toggleEndgame.whenPressed(new ToggleEndgame());
+      break;
+      case OI.ENDGAME_MODE:
+
+      
+      // Bring elevator down and potentially disable it
+      // Change Lift down value
+      // set cargo intake to front
+      // map intake button to bring lift down
+      intakeButton.whileHeld(new CargoIntake_Start());
+      intakeButton.whenReleased(new CargoIntake_Stop());
+      // map HOLD lift button to run rollers
+      // map shoot button to bring lift back
+      
+
+      break;
+      default:
+      intakeButton.whenPressed(new CargoIntake_Start());
+      intakeButton.whenReleased(new CargoIntake_Stop());
+
+      cargoShooter.whenPressed(new CargoShooter_Start());
+      cargoShooter.whenReleased(new CargoShooter_Stop());
+
+      conveyorButton.whenPressed(new Cargo_Transporter_Start());
+      conveyorButton.whenReleased(new CargoTransporter_Stop());
+      break;
+    }
+    
+  }
+
 }
